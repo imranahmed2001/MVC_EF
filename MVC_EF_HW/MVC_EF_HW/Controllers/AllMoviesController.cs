0d338f3c9295912b087160ca679a5b0d513cf130
imran ahmed <ahmed.imran01@ou.edu>
2022-04-19 18:44:01 -0500

    Finished HW 9

---
+++ MVC_EF_HW/MVC_EF_HW/Controllers/AllMoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_EF_HW.Models;
+
+namespace MVC_EF_HW.Controllers
+{
+    public class AllMoviesController : Controller
+    {
+        private DB_128040_practiceEntities db = new DB_128040_practiceEntities();
+
+        // GET: AllMovies
+        public ActionResult Index()
+        {
+            var movies = db.Movies.Include(m => m.Director);
+            return View(movies.ToList());
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC_EF_HW/MVC_EF_HW/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_EF_HW.MvcApplication" Language="C#" %>
